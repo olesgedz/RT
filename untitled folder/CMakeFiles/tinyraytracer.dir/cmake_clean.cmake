72688323d0bf861a0c67d1c133ab4a5eb665f2e9
olesgedz <olesgedz@rambler.ru>
2019-04-22 20:08:30 +0300

    ok
    Former-commit-id: 20fac31f4d28c2cdc41e670e799e95530563330b

---
+++ untitled folder/CMakeFiles/tinyraytracer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o"
+  "tinyraytracer.pdb"
+  "tinyraytracer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinyraytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
