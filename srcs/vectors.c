c03483ae00a86b2f7b3904d138277a9530985759
olesgedz <olesgedz@rambler.ru>
2019-06-04 21:11:15 +0300

    ok, do cone and etc
    Former-commit-id: 34b0935210a80bbeeda3ef95b0ab41990841fa44

--- srcs/vectors.c
+++ srcs/vectors.c
@@ -6,7 +6,7 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 20:23:19 by jblack-b          #+#    #+#             */
-/*   Updated: 2019/06/04 20:39:35 by jblack-b         ###   ########.fr       */
+/*   Updated: 2019/06/04 21:08:23 by jblack-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,139 +14,139 @@
 
 #include "rtv1.h"
 
-/*
-*	Function: creates 3d point from coordinates
-*	Parameters: x y z coodinates of the 3d point as float 
-*	Return: malloced t_vec3, no parameters change
-*
-* ? probably suppose to be t_vector3d or smth
-*/
+// /*
+// *	Function: creates 3d point from coordinates
+// *	Parameters: x y z coodinates of the 3d point as float 
+// *	Return: malloced t_vec3, no parameters change
+// *
+// * ? probably suppose to be t_vector3d or smth
+// */
 
-inline t_vec3 ft_vec3_create(float x, float y, float z)
-{
-	t_vec3 new;
+// inline t_vec3 ft_vec3_create(float x, float y, float z)
+// {
+// 	t_vec3 new;
 
-	new.x = x;
-	new.y = y;
-	new.z = z;
-	return (new);
-}
-/*
-*	Fucntion: sum of two vectors
-*	Parameters: two vectors
-*	Return: sum of two vectors, no parammeters change
-*/
-
-
-inline t_vec3	ft_vec3_sum(t_vec3 a, t_vec3 b)
-{
-	t_vec3 new;
-
-	new.x = (a.x + b.x);
-	new.y = (a.y + b.y);
-	new.z = (a.z + b.z);
-	return (new);
-}
-
-/*
-*	Fucntion: substact two vectors
-*	Parameters: two vectors
-*	Return: difference of two vectors, no parammeters change
-*/
+// 	new.x = x;
+// 	new.y = y;
+// 	new.z = z;
+// 	return (new);
+// }
+// /*
+// *	Fucntion: sum of two vectors
+// *	Parameters: two vectors
+// *	Return: sum of two vectors, no parammeters change
+// */
 
 
-inline t_vec3	ft_vec3_substract(t_vec3 a, t_vec3 b)
-{
-	t_vec3 new;
+// inline t_vec3	ft_vec3_sum(t_vec3 a, t_vec3 b)
+// {
+// 	t_vec3 new;
 
-	new.x = (a.x - b.x);
-	new.y = (a.y - b.y);
-	new.z = (a.z - b.z);
-	return (new);
-}
+// 	new.x = (a.x + b.x);
+// 	new.y = (a.y + b.y);
+// 	new.z = (a.z + b.z);
+// 	return (new);
+// }
 
-/*
-*	Fucntion: prints a 3d point
-*	Parameters: 3d point, no parameters change
-*	Return: none
-* ! printf delete it
-*/
-
-inline void ft_vec3_print(t_vec3 a)
-{
-	printf("x:%f y:%f z:%f\n", a.x, a.y, a.z);
-}
-
-/*
-*	Fucntion: vector multiplication, dot product
-*	Parameters: two vectors no parameters change 
-*	Return: scalar float result of multiplication,
-*/
-inline float ft_vec3_dot_multiply(t_vec3 a, t_vec3 b)
-{
-	return (a.x * b.x + a.y * b.y + a.z * b.z);
-}
-
-/*
-*	Fucntion: vector multiplication by scalar
-*	Parameters: two vectors no parameters change 
-*	Return: t_vec3 vector result of multiplication,
-*/
-
-inline t_vec3 ft_vec3_scalar_multiply(t_vec3 a, float b)
-{
-	return ((t_vec3){a.x * b, a.y * b, a.z * b});
-}
-/*
-*	Fucntion: vector multiplication by vector
-*	Parameters: two vectors no parameters change 
-*	Return: t_vec3 vector result of multiplication,
-*/
-
-inline t_vec3 ft_vec3_cross_multiply(t_vec3 a, t_vec3 b)
-{
-	t_vec3 result;
-
-	result.x = a.y * b.z - a.z * b.y;
-	result.y = a.z * b.x - a.x * b.z;
-	result.z = a.x * b.y - a.y * b.x;
-	return (result);
-}
-
-/*
-*	Fucntion: scalar value of vector
-*	Parameters: vector, no parameters change 
-*	Return: (float) scalar value of a vector
-*/
-
-inline float ft_vec3_norm(t_vec3 vect)
-{
-	return (sqrt(vect.x * vect.x + vect.y * vect.y + vect.z * vect.z));
-}
-
-/*
-*	Fucntion: changes scalar value of a vector
-*	Parameters: vector (changes), needed length
-*	Return: normalized vector
-*/
-
-inline t_vec3 ft_vec3_normalize(t_vec3 vect)
-{
-	float norm = ft_vec3_norm(vect);
-	vect.x = vect.x / norm;
-	vect.y = vect.y / norm;
-	vect.z = vect.z / norm;
-	return (vect);
-}
+// /*
+// *	Fucntion: substact two vectors
+// *	Parameters: two vectors
+// *	Return: difference of two vectors, no parammeters change
+// */
 
 
-/*
-*	Fucntion: projection of vector a on vector b
-*	Parameters: two vectors no parameters change 
-*	Return: float,
-*/
+// inline t_vec3	ft_vec3_substract(t_vec3 a, t_vec3 b)
+// {
+// 	t_vec3 new;
 
-inline float ft_vec3_projection(t_vec3 a, t_vec3 b)
-{
-	return (ft_vec3_dot_multiply(b, a) /  ft_vec3_norm(b));
-}
+// 	new.x = (a.x - b.x);
+// 	new.y = (a.y - b.y);
+// 	new.z = (a.z - b.z);
+// 	return (new);
+// }
+
+// /*
+// *	Fucntion: prints a 3d point
+// *	Parameters: 3d point, no parameters change
+// *	Return: none
+// * ! printf delete it
+// */
+
+// inline void ft_vec3_print(t_vec3 a)
+// {
+// 	printf("x:%f y:%f z:%f\n", a.x, a.y, a.z);
+// }
+
+// /*
+// *	Fucntion: vector multiplication, dot product
+// *	Parameters: two vectors no parameters change 
+// *	Return: scalar float result of multiplication,
+// */
+// inline float ft_vec3_dot_multiply(t_vec3 a, t_vec3 b)
+// {
+// 	return (a.x * b.x + a.y * b.y + a.z * b.z);
+// }
+
+// /*
+// *	Fucntion: vector multiplication by scalar
+// *	Parameters: two vectors no parameters change 
+// *	Return: t_vec3 vector result of multiplication,
+// */
+
+// inline t_vec3 ft_vec3_scalar_multiply(t_vec3 a, float b)
+// {
+// 	return ((t_vec3){a.x * b, a.y * b, a.z * b});
+// }
+// /*
+// *	Fucntion: vector multiplication by vector
+// *	Parameters: two vectors no parameters change 
+// *	Return: t_vec3 vector result of multiplication,
+// */
+
+// inline t_vec3 ft_vec3_cross_multiply(t_vec3 a, t_vec3 b)
+// {
+// 	t_vec3 result;
+
+// 	result.x = a.y * b.z - a.z * b.y;
+// 	result.y = a.z * b.x - a.x * b.z;
+// 	result.z = a.x * b.y - a.y * b.x;
+// 	return (result);
+// }
+
+// /*
+// *	Fucntion: scalar value of vector
+// *	Parameters: vector, no parameters change 
+// *	Return: (float) scalar value of a vector
+// */
+
+// inline float ft_vec3_norm(t_vec3 vect)
+// {
+// 	return (sqrt(vect.x * vect.x + vect.y * vect.y + vect.z * vect.z));
+// }
+
+// /*
+// *	Fucntion: changes scalar value of a vector
+// *	Parameters: vector (changes), needed length
+// *	Return: normalized vector
+// */
+
+// inline t_vec3 ft_vec3_normalize(t_vec3 vect)
+// {
+// 	float norm = ft_vec3_norm(vect);
+// 	vect.x = vect.x / norm;
+// 	vect.y = vect.y / norm;
+// 	vect.z = vect.z / norm;
+// 	return (vect);
+// }
+
+
+// /*
+// *	Fucntion: projection of vector a on vector b
+// *	Parameters: two vectors no parameters change 
+// *	Return: float,
+// */
+
+// inline float ft_vec3_projection(t_vec3 a, t_vec3 b)
+// {
+// 	return (ft_vec3_dot_multiply(b, a) /  ft_vec3_norm(b));
+// }
