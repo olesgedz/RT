0629f1cc2d0a9ffe1ad32b46e9e8ef824649ebb2
lminta <lminta@student.21-school.ru>
2019-09-08 23:24:15 +0300

    normal
    Former-commit-id: 8eebb28bcc33ccec8ceebb2dae47e81c7ad8f1a1

---
+++ gui/build/examples/debug-gizmos/CMakeFiles/debug-gizmos.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/debug-gizmos.dir/debug-gizmos.c.o"
+  "DejaVuSans.ttf"
+  "Fontin-Regular.ttf"
+  "SourceSansPro-Semibold.ttf"
+  "debug-gizmos"
+  "debug-gizmos.pdb"
+  "tileset-alloy.png"
+  "tileset-flat.png"
+  "tileset-futterpedia.png"
+  "tileset.png"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/debug-gizmos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
