c606663105cc47b2741ae2268a9feddd1999ffae
lminta <lminta@student.21-school.ru>
2019-10-29 15:51:44 +0300

    fix restart
    Former-commit-id: ddceb5bc661738fdb99a7428b31046ed1368e302

--- gui/src/info_button.c
+++ gui/src/info_button.c
@@ -6,11 +6,30 @@
 /*   By: lminta <lminta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 17:38:36 by lminta            #+#    #+#             */
-/*   Updated: 2019/10/28 21:33:05 by lminta           ###   ########.fr       */
+/*   Updated: 2019/10/29 15:51:28 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
+
+static void	clicked(KW_Widget *widget, int b)
+{
+	t_gui		*gui;
+	t_game		*game;
+	SDL_Surface	*surface;
+	char		*name;
+
+	b = 0;
+	gui = g_gui(0, 0);
+	game = KW_GetWidgetUserData(widget);
+	surface = SDL_CreateRGBSurface(0, WIN_W, WIN_H, 32, 0, 0, 0, 0);
+	SDL_RenderReadPixels(game->sdl.renderer, NULL,
+	surface->format->format, surface->pixels, surface->pitch);
+	name = filename();
+	IMG_SavePNG(surface, name);
+	free(name);
+	SDL_FreeSurface(surface);
+}
 
 void		show_hide(t_game *game, t_gui *gui)
 {
@@ -51,26 +70,6 @@ static char	*filename(void)
 		name = buff;
 	}
 	return (name);
-}
-
-static void	clicked(KW_Widget *widget, int b)
-{
-	t_gui		*gui;
-	t_game		*game;
-	SDL_Surface	*surface;
-	char		*name;
-
-	b = 0;
-	gui = g_gui(0, 0);
-	game = KW_GetWidgetUserData(widget);
-	surface = SDL_CreateRGBSurface(0, WIN_W, WIN_H, 32, 0, 0, 0, 0);
-	SDL_RenderReadPixels(game->sdl.renderer, NULL,
-	surface->format->format, surface->pixels, surface->pitch);
-	name = filename();
-	IMG_SavePNG(surface, name);
-	free(name);
-	SDL_FreeSurface(surface);
-	gui->flag = 1;
 }
 
 void		semples_to_line(t_game *game, t_gui *gui)
