0e87a7f0d7d2ff717b496ddffc8113f45e0e573f
lminta <lminta@student.21-school.ru>
2019-10-29 18:49:29 +0300

    lmb and rmb
    Former-commit-id: 2a1f5a6b6078f6dfde0b2456f8e0a7d9298ae2d3

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: lminta <lminta@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: Invalid date        by                   #+#    #+#              #
-#    Updated: 2019/10/29 15:42:38 by lminta           ###   ########.fr        #
+#    Updated: 2019/10/29 18:35:35 by lminta           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -82,6 +82,7 @@ SRCS_LIST = main.c\
 			help_fun.c\
 			init_scene.c\
 			render.c\
+			mouse.c\
 			../gui/src/gui_main.c\
 			../gui/src/start_screen.c\
 			../gui/src/surf_tex.c\
--- gui/src/info_button.c
+++ gui/src/info_button.c
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 17:38:36 by lminta            #+#    #+#             */
-/*   Updated: 2019/10/29 15:52:16 by lminta           ###   ########.fr       */
+/*   Updated: 2019/10/29 18:49:02 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,16 +60,18 @@ static void	clicked(KW_Widget *widget, int b)
 	SDL_Surface	*surface;
 	char		*name;
 
-	b = 0;
-	gui = g_gui(0, 0);
 	game = KW_GetWidgetUserData(widget);
-	surface = SDL_CreateRGBSurface(0, WIN_W, WIN_H, 32, 0, 0, 0, 0);
-	SDL_RenderReadPixels(game->sdl.renderer, NULL,
-	surface->format->format, surface->pixels, surface->pitch);
-	name = filename();
-	IMG_SavePNG(surface, name);
-	free(name);
-	SDL_FreeSurface(surface);
+	if (game->ev.button.button != SDL_BUTTON_LEFT)
+		return ;
+		b = 0;
+		gui = g_gui(0, 0);
+		surface = SDL_CreateRGBSurface(0, WIN_W, WIN_H, 32, 0, 0, 0, 0);
+		SDL_RenderReadPixels(game->sdl.renderer, NULL,
+		surface->format->format, surface->pixels, surface->pitch);
+		name = filename();
+		IMG_SavePNG(surface, name);
+		free(name);
+		SDL_FreeSurface(surface);
 }
 
 void		semples_to_line(t_game *game, t_gui *gui)
