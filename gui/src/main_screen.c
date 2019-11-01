71c1f4662d44549a46c1c6a0a87191a27d63ec93
lminta <lminta@student.21-school.ru>
2019-11-01 20:17:12 +0300

    fix multibar
    Former-commit-id: 40b1f483b0c8da04f2803616d86b5b373d90e452

--- gui/src/main_screen.c
+++ gui/src/main_screen.c
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 16:46:11 by lminta            #+#    #+#             */
-/*   Updated: 2019/11/01 15:54:58 by lminta           ###   ########.fr       */
+/*   Updated: 2019/11/01 20:13:43 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 void	main_screen(t_gui *gui, t_game *game)
 {
-	KW_HideWidget(gui->s_s.frame);
 	obj_select(gui, game->gpu.objects, game->obj_quantity);
-	KW_HideWidget(gui->o_s.frame);
 }
 
 void	main_screen_free(t_gui *gui)
