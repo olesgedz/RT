9bb1f9d00c637cdd91f9c289b82cd9a3011488b2
lminta <lminta@student.21-school.ru>
2019-11-05 14:06:55 +0300

    small fix
    Former-commit-id: d99a969d14ce0433a7f1d8a7a7d2eef09d602209

--- gui/src/gui_bar.c
+++ gui/src/gui_bar.c
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 19:10:18 by lminta            #+#    #+#             */
-/*   Updated: 2019/10/31 19:28:07 by lminta           ###   ########.fr       */
+/*   Updated: 2019/11/05 14:05:45 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,4 @@ void		gui_bar(t_game *game, t_gui *gui)
 	gui->g_b.frect = (KW_Rect){0, 0, WIN_W, FR_FZ / 5.};
 	gui->g_b.frame = KW_CreateFrame(gui->gui, NULL, &gui->g_b.frect);
 	buttons(game, gui);
-	//KW_AddWidgetMouseDownHandler(gui->g_b.buttons, clicked);
-	//KW_SetWidgetUserData(gui->g_b.buttons[0], (void *)game);
 }
--- gui/src/main_screen.c
+++ gui/src/main_screen.c
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 16:46:11 by lminta            #+#    #+#             */
-/*   Updated: 2019/11/01 20:13:43 by lminta           ###   ########.fr       */
+/*   Updated: 2019/11/05 14:06:20 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 void	main_screen(t_gui *gui, t_game *game)
 {
 	obj_select(gui, game->gpu.objects, game->obj_quantity);
+	if (!gui->o_s.show)
+		KW_HideWidget(gui->o_s.frame);
 }
 
 void	main_screen_free(t_gui *gui)
