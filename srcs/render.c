02e3e9bfa9ab995369cba2b341d443c286c26a90
lminta <lminta@student.21-school.ru>
2019-11-06 19:47:28 +0300

    pushsia
    Former-commit-id: 03c33e03a0c0b478daef77c347a8bff8db27562d

--- srcs/render.c
+++ srcs/render.c
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 14:54:28 by lminta            #+#    #+#             */
-/*   Updated: 2019/11/06 19:38:08 by lminta           ###   ########.fr       */
+/*   Updated: 2019/11/06 19:42:49 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,10 +71,9 @@ void			main_render(t_game *game, t_gui *gui)
 		ft_render(game, gui);
 		screen_present(game, gui);
 		time = (SDL_GetTicks() - time0) / 1000.;
-		if (frames % 10 == 0)
-			printf("%f\n", frames / time);
-		if (frames % 100 == 0)
+		if (frames % 50 == 0)
 		{
+			printf("%f\n", frames / time);
 			time0 = SDL_GetTicks();
 			frames = 1;
 		}
