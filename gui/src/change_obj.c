d9d0477e33c6a8c842f450d605e0c4f5b2b5f06f
lminta <lminta@student.21-school.ru>
2019-11-18 21:39:17 +0300

    add obj begining
    Former-commit-id: 60f5c28eae68d22f37b88c31d1002cb3418a5ac3

--- gui/src/change_obj.c
+++ gui/src/change_obj.c
@@ -6,13 +6,33 @@
 /*   By: lminta <lminta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 20:10:09 by lminta            #+#    #+#             */
-/*   Updated: 2019/11/14 20:30:07 by lminta           ###   ########.fr       */
+/*   Updated: 2019/11/18 21:08:36 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-void	change_plane(t_game *game, t_gui *gui, t_obj *obj)
+void	change_plane(/*t_game *game, */t_gui *gui, t_obj *obj)
 {
-	
+	obj_same(gui, obj);
+}
+
+void	change_sphere(/*t_game *game, */t_gui *gui, t_obj *obj)
+{
+	obj_same(gui, obj);
+}
+
+void	change_cylin(/*t_game *game, */t_gui *gui, t_obj *obj)
+{
+	obj_same(gui, obj);
+}
+
+void	change_cone(/*t_game *game, */t_gui *gui, t_obj *obj)
+{
+	obj_same(gui, obj);
+}
+
+void	change_trian(/*t_game *game, */t_gui *gui, t_obj *obj)
+{
+	obj_same(gui, obj);
 }
--- includes/rt.h
+++ includes/rt.h
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 14:49:06 by lminta            #+#    #+#             */
-/*   Updated: 2019/11/14 19:24:00 by lminta           ###   ########.fr       */
+/*   Updated: 2019/11/18 21:19:41 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -275,5 +275,14 @@ void					mouse_motion(t_game *game, t_gui *gui);
 void					free_list(t_game *game);
 void					add_obj(t_game *game, t_gui *gui);
 void					obj_type(t_game *game, t_gui *gui);
+void					change_plane(/*t_game *game,*/ t_gui *gui, t_obj *obj);
+void					change_sphere(/*t_game *game, */t_gui *gui, t_obj *obj);
+void					change_cylin(/*t_game *game, */t_gui *gui, t_obj *obj);
+void					change_cone(/*t_game *game, */t_gui *gui, t_obj *obj);
+void					change_trian(/*t_game *game, */t_gui *gui, t_obj *obj);
+void 					obj_if(t_gui *gui, t_obj *obj);
+char					*fill_name_mass(t_obj *obj, int num);
+void					obj_same(t_gui *gui, t_obj *obj);
+void					visibility_name(KW_Widget *widget, t_obj *obj);
 
 #endif
