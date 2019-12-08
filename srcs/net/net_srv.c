48491d81f54a12ce176a673f930aca754ffccfa5
lminta <lminta@student.21-school.ru>
2019-12-08 21:55:19 +0300

    q
    Former-commit-id: bf2e9745e555c5e09bb614f41a2aba67fc8a0c62

--- srcs/net/net_srv.c
+++ srcs/net/net_srv.c
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/07 21:14:07 by lminta            #+#    #+#             */
-/*   Updated: 2019/12/08 21:52:25 by lminta           ###   ########.fr       */
+/*   Updated: 2019/12/08 21:54:14 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,8 +60,8 @@ static void	client_side(t_game *game, t_gui *gui)
 	buff = ft_strdup(message);
 	map = ft_strstr(buff, ".json") + 5;
 	// name = malloc(message - map + 1);
-	printf("%d\n", (int)(buff - map));
-	name = ft_strncpy(name, buff, buff - map);
+	printf("%d\n", (int)(map - buff));
+	name = ft_strncpy(name, buff, map - buff);
 
 
 	// if (!ft_strcmp(&message[ft_strlen(message) - 5], ".json"))
