843611833cc117fe316022fba13f5cfa45243da6
lminta <lminta@student.21-school.ru>
2019-12-10 18:08:46 +0300

    seg fix + large map send
    Former-commit-id: 3e9cbbf6692940dd2088c32ffd72b36f8431462e

--- includes/rt.h
+++ includes/rt.h
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 14:49:06 by lminta            #+#    #+#             */
-/*   Updated: 2019/12/09 16:45:10 by lminta           ###   ########.fr       */
+/*   Updated: 2019/12/10 18:01:46 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -491,4 +491,5 @@ void					parse_rest(const cJSON *object, t_obj *obj, t_json *parse);
 void					parse_triangle_vert(const cJSON *object, t_obj *obj, t_json *parse);
 void					prepare_data(char ***data, char *line);
 char					*make_string(char *name, int smpls, int fd);
+void					scene_click(KW_Widget *widget, int b);
 #endif
--- srcs/net/net_srv.c
+++ srcs/net/net_srv.c
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/07 21:14:07 by lminta            #+#    #+#             */
-/*   Updated: 2019/12/10 17:48:53 by lminta           ###   ########.fr       */
+/*   Updated: 2019/12/10 18:08:12 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,8 @@ static void	client_side(t_game *game, t_gui *gui)
 	}
 	fclose(fp);
 	free(gui->av);
+	scene_select(gui, -1, 0);
+	scene_click(0, 0);
 	gui->av = buff[1];
 	gui->quit = 1;
 	free(buff[0]);
