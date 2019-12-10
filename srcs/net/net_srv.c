426dab3d72c1fe1c5414da550117d31ef650d9ec
lminta <lminta@student.21-school.ru>
2019-12-10 17:49:27 +0300

    fix again
    Former-commit-id: 3ba81c2afaf4c4d21da6aa800d6fc71e08a2acea

--- srcs/net/net_srv.c
+++ srcs/net/net_srv.c
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/07 21:14:07 by lminta            #+#    #+#             */
-/*   Updated: 2019/12/10 17:39:29 by lminta           ###   ########.fr       */
+/*   Updated: 2019/12/10 17:48:53 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,10 @@ static void	client_side(t_game *game, t_gui *gui)
 	char		message[FILE_SIZE];
 	FILE		*fp;
 	char		**buff;
-	int test, len;
+	int 		len;
+	int			current;
 
 	len = SDLNet_TCP_Recv(gui->n.tcpsock, message, FILE_SIZE);
-	//printf("R - %d - %d\n", test, FILE_SIZE);
 	if (!ft_strcmp(message, "ping!"))
 		return ;
 	buff = ft_strsplit(message, '|');
@@ -61,8 +61,12 @@ static void	client_side(t_game *game, t_gui *gui)
 	game->samples_to_do = ft_atoi(buff[2]);
 	len = ft_atoi(buff[0]) - len + ft_strlen(buff[0]);
 	fprintf(fp, "%s", buff[3]);
-	while ((len -= SDLNet_TCP_Recv(gui->n.tcpsock, message, FILE_SIZE)) > 0)
-		fprintf(fp, "%s", buff[3]);
+	while (len > 0)
+	{
+		current = SDLNet_TCP_Recv(gui->n.tcpsock, message, FILE_SIZE);
+		fprintf(fp, "%s", message);
+		len -= current;
+	}
 	fclose(fp);
 	free(gui->av);
 	gui->av = buff[1];
