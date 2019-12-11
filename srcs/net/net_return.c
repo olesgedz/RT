0e9450a32efb144a34243145869f4a3ffbe9d980
lminta <lminta@student.21-school.ru>
2019-12-11 14:41:46 +0300

    test recv
    Former-commit-id: 735609e572a537cc5510481d80f62406714c2adc

--- srcs/net/net_return.c
+++ srcs/net/net_return.c
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 21:33:44 by lminta            #+#    #+#             */
-/*   Updated: 2019/12/10 23:14:40 by lminta           ###   ########.fr       */
+/*   Updated: 2019/12/11 14:41:25 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,21 @@ static void	serv_side(t_game *game, t_gui *gui, int len)
 	int		current;
 	int		i;
 	int		all;
-	char	buff[WIN_W];
+	char	buff[sizeof(cl_float3) * WIN_W];
 
 	i = -1;
 	while (++i < gui->n.clients)
 	{
+		game->cl_info->ret = cl_read(game->cl_info,
+	game->cl_info->progs[0].krls[0].args[2], len, game->gpu.vec_temp);
 		all = 0;
 		while (all <= len)
 		{
 			current = 0;
-			current = SDLNet_TCP_Recv(gui->n.client[i], buff, WIN_W);
-			ft_memcpy(game->gpu.vec_temp1 + all, buff, current);
+			current = SDLNet_TCP_Recv(gui->n.client[i],
+			buff, sizeof(cl_float3) * WIN_W);
+			printf("%d-%d", current, all);
+			//ft_memcpy(game->gpu.vec_temp1 + all, buff, current);
 			all += current;
 		}
 	}
