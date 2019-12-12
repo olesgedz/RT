72ab2b4d740d1f1905808f88920de12672bc5a3d
lminta <lminta@student.21-school.ru>
2019-12-12 17:22:48 +0300

    test
    Former-commit-id: bfd4a505fc749865a2e9911cdd49a309bc2d449e

--- srcs/net/net_return.c
+++ srcs/net/net_return.c
@@ -6,7 +6,7 @@
 /*   By: lminta <lminta@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 21:33:44 by lminta            #+#    #+#             */
-/*   Updated: 2019/12/12 15:48:35 by lminta           ###   ########.fr       */
+/*   Updated: 2019/12/12 17:20:24 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,6 @@ static void			serv_side(t_game *game, t_gui *gui, int len, cl_float3 *tmp)
 
 	i = -1;
 	game->gpu.samples *= gui->n.clients + 1;
-	printf(">>>>%f, %f, %f \n", tmp[1000].s[0], tmp[1000].s[2], tmp[1000].s[2]);
 	while (++i < gui->n.clients)
 	{
 		all = 0;
@@ -57,10 +56,12 @@ static void			serv_side(t_game *game, t_gui *gui, int len, cl_float3 *tmp)
 			current / sizeof(cl_float3));
 			all += current;
 			if (current % sizeof(cl_float3))
+			{
 				all++;
+				printf("+++++\n");
+			}
 		}
 	}
-	printf(">>>>%f, %f, %f \n", tmp[1000].s[0], tmp[1000].s[2], tmp[1000].s[2]);
 	game->cl_info->ret = cl_write(game->cl_info,
 	game->cl_info->progs[0].krls[0].args[2], len, tmp);
 	ft_run_kernel(game, &game->cl_info->progs[0].krls[0], WIN_W, WIN_H);
